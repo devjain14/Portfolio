85cadbd8a83e51c77e9dace4ac72ef8c289af42b
Dev_Jain <Dev_Jain>
2017-04-25 15:22:08 +0530

    Orange them with blue animated strips

---
+++ js/main.js
@@ -0,0 +1,12 @@
+    function aboutMe(){
+                $("#skills").hide(200);
+                $("#aboutMe").show(200);
+                $("#aboutbutton1").css("color","#e55102");
+                $("#aboutbutton2").css("color","black");
+            }
+    function skills(){
+                $("#aboutMe").hide(200);
+                $("#skills").show(200);
+                $("#aboutbutton1").css("color","black");
+                $("#aboutbutton2").css("color","#e55102");
+            }
