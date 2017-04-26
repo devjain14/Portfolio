b3941a59150602826b48fcf3fbfd7232768bf046
Dev_Jain <Dev_Jain>
2017-04-26 15:46:58 +0530

    [Working] Everything is working. FINAL

--- js/main.js
+++ js/main.js
@@ -10,3 +10,11 @@
                 $("#aboutbutton1").css("color","black");
                 $("#aboutbutton2").css("color","#e55102");
             }
+            function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
